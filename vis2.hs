0bcb6a95db748fbdadc8e3fd0be2aa2513a840de
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2010-08-25 21:48:07 +0200

    Fix a number of compiler warnings

--- vis2.hs
+++ vis2.hs
@@ -7,15 +7,15 @@ main = do
     (program_name, _) <- getArgsAndInitialize
     initialDisplayMode $= [DoubleBuffered, RGBAMode, WithDepthBuffer]
     initialWindowSize $= Size 1000 500
-    createWindow program_name
+    _ <- createWindow program_name
     displayCallback $= display
-    reshapeCallback $= Just reshape    
+    reshapeCallback $= Just reshape
     clearColor $= Color4 0.0 0.0 0.0 1.0
     depthFunc $= Just Less
     matrixMode $= Projection
     loadIdentity
     ortho 0.0 1000.0 500.0 0.0 (-1.0) (1.0)
-    matrixMode $= Modelview 0    
+    matrixMode $= Modelview 0
     mainLoop
 
 node :: IO ()
@@ -27,10 +27,10 @@ node = do
         vertex $ Vertex3 (-1.0::GLdouble) (-1.0) 0.0
 
 drawNode :: GLdouble -> (Vector3 GLdouble) -> (Color4 GLdouble) -> IO ()
-drawNode size position color_val = do
+drawNode size pos col = do
     unsafePreservingMatrix $ do
-        color color_val
-        translate position
+        color col
+        translate pos
         scale size size size
         node
 
@@ -47,12 +47,12 @@ display = do
     gen_1 <- newStdGen
     let (r, gen_2) = randomR (0.0::Double, 1.0) gen_1
         (g, gen_3) = randomR (0.0::Double, 1.0) gen_2
-        (b, gen_4) = randomR (0.0::Double, 1.0) gen_3
-    drawNode 10.5 position (color r g b)
+        (b, _) = randomR (0.0::Double, 1.0) gen_3
+    drawNode 10.5 pos (col r g b)
     flush
     swapBuffers
     where r_val r = (r + 1.0) / 2.0
           g_val g = (g + 1.0) / 2.0
           b_val b = (b + 1.0) / 2.0
-          color r g b = Color4 (r_val r) (g_val g) (b_val b) 1.0
-          position = Vector3 400.0 150.0 0.0
+          col r g b = Color4 (r_val r) (g_val g) (b_val b) 1.0
+          pos = Vector3 400.0 150.0 0.0
